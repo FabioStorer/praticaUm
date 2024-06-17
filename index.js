c9e2a7943dfd7550072074a59ba96d0a8fe69c46
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-17 17:24:27 -0300

    feat: adicionado a busca pelo usuario usando email

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Prática 1. Atividade para buscar objeto por um email.');
+let cadastro = [{
+    nome: 'Fabio',
+    email: 'fabioemail@hotmail.com',
+    idade: 29
+}, {
+    nome: 'Eduarda',
+    email: 'eduardaemail@hotmail.com',
+    idade: 28
+}, {
+    nome: 'Roberto',
+    email: 'robertoemail@hotmail.com',
+    idade: 33
+}];
+let input = '';
+let salvar = '';
+
+console.log('Olá. Você está fazendo uma busca em um banco de dados de um cadastro.');
+console.log('Digite um email válido para verificar se o usuário está cadastrado.');
+console.log('Ou digite "Sair" para finalizar a busca.');
+
+process.stdin.on('data', function (data) {
+    input = data.toString().trim();
+
+    if (input == 'Sair' || input == 'sair') {
+        process.exit();
+    }
+
+    for (let i = 0; i < cadastro.length; i++) {
+        if (cadastro[i].email === input) {
+            salvar = cadastro[i];
+        }
+    }
+
+    if (salvar) {
+        console.log('Essa pessoa está cadastrada.');
+        salvar = '';
+    } else {
+        console.log('Essa pessoa não foi encontrada.');
+        salvar = '';
+    }
+})
